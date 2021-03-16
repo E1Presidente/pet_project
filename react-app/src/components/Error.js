33682a7a3889dd1795499171673290ec4c60d99e
Vadim <el.presidente.de.cuba@gmail.com>
2021-03-16 12:36:16 +0200

    minor changes

--- react-app/src/components/Error.js
+++ react-app/src/components/Error.js
@@ -4,10 +4,10 @@ function Error() {
   return (
     <>
       <Container className="pt-5">
-        <Row className="justify-content-sm-center m-4">
+        <Row className="justify-content-sm-center">
           <Col md="auto"><h1>ERROR</h1></Col>
         </Row>
-        <Row className="justify-content-sm-center m-4">
+        <Row className="justify-content-sm-center">
           <Col md="auto"><h3>No such page!</h3></Col>
         </Row>
       </Container>
